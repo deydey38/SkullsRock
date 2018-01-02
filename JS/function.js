068158b8cde002144a50f0e54b8ce2e2d2d81370
deydey38 <andre.girin38@gmail.com>
2018-01-02 23:54:48 +0100

    opacity on scroll

--- JS/function.js
+++ JS/function.js
@@ -1,11 +1,15 @@
 window.onscroll = function() {myFunction()};
 
+var navbar = document.getElementById("navbar");
+var bandName = document.getElementById("bandName");
 
 function myFunction() {
-  if (window.pageYOffset >= 20) {
+  if (window.pageYOffset >= 700) {
     navbar.classList.add("sticky");
+    bandName.classList.add("sticky-title");
   } else {
     navbar.classList.remove("sticky");
+    bandName.classList.remove("sticky-title");
   }
 }
 
@@ -14,5 +18,9 @@ $(window).scroll(function(){
   console.log(wScroll);
   $(".band").css({
     'transform':'translate(0px, '+wScroll+'%)'
+  });
+  var op = 0.3+wScroll/1000;
+  $(".filter").css({
+    'background-color' : 'rgba(0, 0, 0, '+op+')'
   });
 });
