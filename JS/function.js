f8daabd8b827192a529f426c14b5bf54919d961e
Andre Girin <Andre.Girin@hardis.fr>
2018-01-03 14:28:05 +0100

    add is showing

--- JS/function.js
+++ JS/function.js
@@ -1,26 +1,37 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
-var bandName = document.getElementById("bandName");
+var logo = document.getElementById("logo");
 
 function myFunction() {
-  if (window.pageYOffset >= 700) {
+  if (window.pageYOffset >= 10) {
     navbar.classList.add("sticky");
-    bandName.classList.add("sticky-title");
+    logo.classList.add("sticky-title");
   } else {
     navbar.classList.remove("sticky");
-    bandName.classList.remove("sticky-title");
+    logo.classList.remove("sticky-title");
   }
 }
 
 $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
-  console.log(wScroll);
-  $(".band").css({
-    'transform':'translate(0px, '+wScroll+'%)'
-  });
   var op = 0.3+wScroll/1000;
+
+  console.log(wScroll);
+  $(".logo").css({
+    'transform':'translate(0px, '+wScroll/1.75+'px)'
+  });
+
   $(".filter").css({
     'background-color' : 'rgba(0, 0, 0, '+op+')'
   });
+
+  if(wScroll > $('.groupe-h1').offset().top - $(window).height()/1.5){
+    $('.groupe-h1').addClass("is-showing");
+  }
+
+  if(wScroll > $('.row').offset().top - $(window).height()/1.5){
+    $('.row').addClass("is-showing");
+  }
+
 });
